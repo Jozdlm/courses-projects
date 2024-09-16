9e6c7e09c5da399ca1efd1144aa9f2f04870e96d
Jozuan Lopez <jozuanlopez@gmail.com>
2024-09-16 15:29:02 -0600

    Test the 11-async-await file

---
+++ counter-app/src/scripts/11-async-await.js
@@ -0,0 +1,14 @@
+export const getGifImg = async () => {
+  try {
+    const apiKey = "uGypZ1DrIpzfiR7CVm6nxlUxct4jDmt7";
+    const resp = await fetch(
+      `http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`
+    );
+    const { data } = await resp.json();
+
+    const { url } = data.images.original;
+    return url;
+  } catch (error) {
+    return "Image not founded";
+  }
+};
---
+++ counter-app/tests/scripts/11-async-await.test.js
@@ -0,0 +1,9 @@
+import { getGifImg } from "../../src/scripts/11-async-await";
+
+describe("Test in the 11-async-await file", () => {
+  test("should getGifImg return a gif image URL", async () => {
+    const url = await getGifImg();
+
+    expect(url).toEqual(expect.not.stringContaining("Image not founded"));
+  });
+});
